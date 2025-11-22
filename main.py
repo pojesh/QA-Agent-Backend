f4bce5a7b64bc8512e5b2139da448300aae878a9
POJESH <rpojesh1@gmail.com>
2025-11-23 01:57:36 +0530

    fix: fix code

--- main.py
+++ main.py
@@ -43,4 +43,4 @@ app.include_router(session.router, prefix=f"{settings.API_PREFIX}/session", tags
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("backend.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
